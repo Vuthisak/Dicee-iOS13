d22fe38cbb0201e54c44fcc2d49607fb63fe1d9f
khyvuthisak <khyvuthisak@gmail.com>
2022-07-30 22:38:08 +0700

    update action for roll button

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -11,13 +11,17 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = UIImage(named: "DiceSix")
+        diceImageView2.image = UIImage(named:"DiceFour")
+    }
+    
 }
 
